f8b70205cfdb326d62086adddb1f3bd4b869955c
aslut00 <42335251+aslut00@users.noreply.github.com>
2018-09-28 13:59:32 +0900

    upload

---
+++ app/src/main/java/com/example/ahn/backingps/MainActivity.java
@@ -0,0 +1,307 @@
+package com.example.ahn.backingps;
+
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.ContentValues;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.AsyncTask;
+import android.os.Build;
+import android.os.StrictMode;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.webkit.WebView;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.ToggleButton;
+
+
+import java.net.URL;
+import java.net.URLConnection;
+
+import static android.os.Build.VERSION_CODES.M;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView tv;
+    ToggleButton tb;
+    private TextView tv_outPut;
+    String ip_addr = "";
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        tv_outPut = (TextView) findViewById(R.id.tv_outPut);
+
+
+
+
+        /////////////////권한요청/////////////////////
+        checkPermissionF();
+
+
+        tv = (TextView) findViewById(R.id.textView2);
+        tv.setText("위치정보 미수신중");
+
+        tb = (ToggleButton)findViewById(R.id.toggle1);
+
+        // LocationManager 객체를 얻어온다
+        final LocationManager lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+
+        tb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try{
+                    if(tb.isChecked()){
+                        tv.setText("수신중..");
+                        // GPS 제공자의 정보가 바뀌면 콜백하도록 리스너 등록하기~!!!
+                        lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, // 등록할 위치제공자
+                                100, // 통지사이의 최소 시간간격 (miliSecond)
+                                1, // 통지사이의 최소 변경거리 (m)
+                                mLocationListener);
+                        lm.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, // 등록할 위치제공자
+                                100, // 통지사이의 최소 시간간격 (miliSecond)
+                                1, // 통지사이의 최소 변경거리 (m)
+                                mLocationListener);
+                    }else{
+                        tv.setText("위치정보 미수신중");
+                        lm.removeUpdates(mLocationListener);  //  미수신할때는 반드시 자원해체를 해주어야 한다.
+                    }
+                }catch(SecurityException ex){
+                }
+            }
+        });
+
+
+    }
+
+
+
+    public class NetworkTask extends AsyncTask<Void, Void, String> {
+
+        private String url;
+        private ContentValues values;
+
+        public NetworkTask(String url, ContentValues values) {
+
+            this.url = url;
+            this.values = values;
+        }
+
+        @Override
+        protected String doInBackground(Void... params) {
+
+            String result; // 요청 결과를 저장할 변수.
+            RequestHttpURLConnection requestHttpURLConnection = new RequestHttpURLConnection();
+            result = requestHttpURLConnection.request(url, values); // 해당 URL로 부터 결과물을 얻어온다.
+
+            return result;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+
+            //doInBackground()로 부터 리턴된 값이 onPostExecute()의 매개변수로 넘어오므로 s를 출력한다.
+            tv_outPut.setText(s);
+        }
+    }
+
+
+
+    private void checkPermissionF() {
+
+        if (android.os.Build.VERSION.SDK_INT >= M) {
+            // only for LOLLIPOP and newer versions
+            System.out.println("Hello Marshmallow (마시멜로우)");
+            int permissionResult = getApplicationContext().checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE);
+
+            if (permissionResult == PackageManager.PERMISSION_DENIED) {
+                //요청한 권한( WRITE_EXTERNAL_STORAGE )이 없을 때..거부일때...
+                        /* 사용자가 WRITE_EXTERNAL_STORAGE 권한을 한번이라도 거부한 적이 있는 지 조사한다.
+                        * 거부한 이력이 한번이라도 있다면, true를 리턴한다.
+                        */
+                if (shouldShowRequestPermissionRationale(Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+
+
+                    AlertDialog.Builder dialog = new AlertDialog.Builder(getApplicationContext());
+                    dialog.setTitle("권한이 필요합니다.")
+                            .setMessage("단말기의 파일쓰기 권한이 필요합니다.\\n계속하시겠습니까?")
+                            .setPositiveButton("확인", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+
+                                    if (Build.VERSION.SDK_INT >= M) {
+
+                                        System.out.println("감사합니다. 권한을 허락했네요 (마시멜로우)");
+                              /*          requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE,Manifest.permission.ACCESS_FINE_LOCATION,Manifest.permission.ACCESS_COARSE_LOCATION,
+                                                Manifest.permission.READ_PHONE_STATE,Manifest.permission.READ_EXTERNAL_STORAGE}, 1);*/
+                                        requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE,Manifest.permission.ACCESS_FINE_LOCATION,Manifest.permission.ACCESS_COARSE_LOCATION,Manifest.permission.READ_EXTERNAL_STORAGE}, 1);
+                                    }
+
+                                }
+                            })
+                            .setNegativeButton("취소", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    Toast.makeText(getApplicationContext(),"권한 요청 취소", Toast.LENGTH_SHORT).show();
+                                }
+                            })
+                            .create()
+                            .show();
+
+                    //최초로 권한을 요청할 때.
+                } else {
+                    System.out.println("최초로 권한을 요청할 때. (마시멜로우)");
+                    requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE,Manifest.permission.ACCESS_FINE_LOCATION,Manifest.permission.ACCESS_COARSE_LOCATION,Manifest.permission.READ_EXTERNAL_STORAGE}, 1);
+                    //        getThumbInfo();
+                }
+            }else{
+                //권한이 있을 때.
+                //       getThumbInfo();
+            }
+
+        } else {
+            System.out.println("(마시멜로우 이하 버전입니다.)");
+            //   getThumbInfo();
+        }
+
+    }
+    /**
+     * 사용자가 권한을 허용했는지 거부했는지 체크
+     * @param requestCode   1번
+     * @param permissions   개발자가 요청한 권한들
+     * @param grantResults  권한에 대한 응답들
+     *                    permissions와 grantResults는 인덱스 별로 매칭된다.
+     */
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+
+        if (requestCode == 1) {
+            /* 요청한 권한을 사용자가 "허용"했다면 인텐트를 띄워라
+                내가 요청한 게 하나밖에 없기 때문에. 원래 같으면 for문을 돈다.*/
+/*            requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE,Manifest.permission.ACCESS_FINE_LOCATION,Manifest.permission.ACCESS_COARSE_LOCATION,
+                    Manifest.permission.READ_PHONE_STATE,Manifest.permission.READ_EXTERNAL_STORAGE}, 1);*/
+
+            for(int i = 0 ; i < permissions.length ; i++) {
+                if (grantResults.length > 0 && grantResults[i] == PackageManager.PERMISSION_GRANTED) {
+                    if (ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                        System.out.println("onRequestPermissionsResult WRITE_EXTERNAL_STORAGE ( 권한 성공 ) ");
+                    }
+                    if (ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                        System.out.println("onRequestPermissionsResult ACCESS_FINE_LOCATION ( 권한 성공 ) ");
+                    }
+                    if (ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                        System.out.println("onRequestPermissionsResult ACCESS_COARSE_LOCATION ( 권한 성공 ) ");
+                    }
+          /*          if (ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
+                        System.out.println("onRequestPermissionsResult READ_PHONE_STATE ( 권한 성공 ) ");
+                    }*/
+                    if (ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                        System.out.println("onRequestPermissionsResult READ_EXTERNAL_STORAGE ( 권한 성공 ) ");
+                    }
+                }
+
+
+            }
+
+        } else {
+            System.out.println("onRequestPermissionsResult ( 권한 거부) ");
+            Toast.makeText(getApplicationContext(), "요청 권한 거부", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+
+
+
+
+    @SuppressLint("NewApi")
+    private final LocationListener mLocationListener = new LocationListener() {
+        String lati="";
+        String longi="";
+        public void onLocationChanged(Location location) {
+            //여기서 위치값이 갱신되면 이벤트가 발생한다.
+            //값은 Location 형태로 리턴되며 좌표 출력 방법은 다음과 같다.
+
+            Log.d("test", "onLocationChanged, location:" + location);
+            double longitude = location.getLongitude(); //경도
+            double latitude = location.getLatitude();   //위도
+            double altitude = location.getAltitude();   //고도
+            float accuracy = location.getAccuracy();    //정확도
+            String provider = location.getProvider();   //위치제공자
+            //Gps 위치제공자에 의한 위치변화. 오차범위가 좁다.
+            //Network 위치제공자에 의한 위치변화
+            //Network 위치는 Gps에 비해 정확도가 많이 떨어진다.
+            tv.setText("위치정보 : " + provider + "\n위도 : " + longitude + "\n경도 : " + latitude
+                    + "\n고도 : " + altitude + "\n정확도 : "  + accuracy);
+
+
+            lati = Double.toString(latitude);
+            longi = Double.toString(longitude);
+
+
+            EditText editText = (EditText) findViewById(R.id.edit_text);
+
+            ip_addr = editText.getText().toString();
+
+
+            new Thread(new Runnable() {
+                @Override public void run() {
+                    try {
+                        // URL 설정.
+                        String urltest = "http://" +"ec2-13-209-77-169.ap-northeast-2.compute.amazonaws.com:5000/gps" + "?latitude=" + lati + "&longitude=" + longi;
+                        System.out.println(urltest);
+                        URL url = new URL(urltest);
+                        URLConnection conn = url.openConnection();
+                        conn.getInputStream();
+                        Log.i("msg","go");
+                    } catch (Exception e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                        Log.i("msg","no");
+                    }
+
+                }
+            }).start();
+
+
+        }
+        public void onProviderDisabled(String provider) {
+            // Disabled시
+            Log.d("test", "onProviderDisabled, provider:" + provider);
+        }
+
+        public void onProviderEnabled(String provider) {
+            // Enabled시
+            Log.d("test", "onProviderEnabled, provider:" + provider);
+        }
+
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+            // 변경시
+            Log.d("test", "onStatusChanged, provider:" + provider + ", status:" + status + " ,Bundle:" + extras);
+        }
+    };
+
+
+
+
+
+
+}
